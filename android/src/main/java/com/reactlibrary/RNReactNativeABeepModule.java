c2170d0c101b9fdeae6f558db146ec0d85c79fea
Nguyễn Châu Thành Thiện <thiensmail96@gmail.com>
2019-01-04 12:59:40 +0700

    Fix bug

--- android/src/main/java/com/reactlibrary/RNReactNativeABeepModule.java
+++ android/src/main/java/com/reactlibrary/RNReactNativeABeepModule.java
@@ -30,7 +30,7 @@ public class RNReactNativeABeepModule extends ReactContextBaseJavaModule {
       //toneGen1.startTone(soundID);
       audioManager = (AudioManager) reactContext.getSystemService(
               reactContext.AUDIO_SERVICE);
-      audioManager.playSoundEffect(AudioManager.FX_KEY_CLICK, 10);
+      audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD, 10);
     }
 
   @ReactMethod
